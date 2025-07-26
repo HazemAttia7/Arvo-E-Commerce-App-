7053d2a2bb015d368c2e18c869554769f1ba9bf9
HazemAttia7 <lolhazem41@gmail.com>
2025-07-26 13:07:54 +0300

    added favorite button to card details section

--- lib/Home Page/widgets/Card Details/product_details_section.dart
+++ lib/Home Page/widgets/Card Details/product_details_section.dart
@@ -5,78 +5,112 @@ import 'package:e_commerce_app/Home%20Page/widgets/custom_playfair_text.dart';
 import 'package:e_commerce_app/Home%20Page/widgets/clickable_modern_underlined_text.dart';
 import 'package:e_commerce_app/Home%20Page/widgets/price_text.dart';
 import 'package:e_commerce_app/Main%20Screen/widgets/custom_shopping_cart_icon_button.dart';
+import 'package:e_commerce_app/global/helper/data.dart';
 import 'package:e_commerce_app/global/models/product_model.dart';
 import 'package:e_commerce_app/global/services/products_service.dart';
 import 'package:flutter/material.dart';
 
-class ProductDetailsSection extends StatelessWidget {
+class ProductDetailsSection extends StatefulWidget {
   const ProductDetailsSection({super.key, required this.product});
 
   final ProductModel product;
 
   @override
+  State<ProductDetailsSection> createState() => _ProductDetailsSectionState();
+}
+
+class _ProductDetailsSectionState extends State<ProductDetailsSection> {
+  late bool isFavorite;
+  @override
+  void initState() {
+    isFavorite = currentUser?.favList.contains(widget.product) ?? false;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
+    return Stack(
       children: [
-        Expanded(
-          child: CustomImageWithIndicatorWhileLoading(
-            image: product.images[1] ?? product.images[0],
-          ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (product.brand != null)
-                CustomPlayFairText(fontSize: 22, text: product.brand!),
-              Text(
-                product.title,
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
+        Row(
+          children: [
+            Expanded(
+              child: CustomImageWithIndicatorWhileLoading(
+                image: widget.product.images[1] ?? widget.product.images[0],
               ),
-              Text(
-                product.category.replaceAll("-", " ").toUpperCase(),
-                style: const TextStyle(
-                  fontSize: 12,
-                  color: Color(0xff848484),
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              const SizedBox(height: 5),
-              PriceText(
-                price: product.price,
-                discountPercentage: product.discountPercentage,
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(right: 95),
-                child: BuyNowButton(
-                  onTap: () async {
-                    await ProductsService().addProductToShoppingCart(
-                      context,
-                      product,
-                    );
-                    final productsMap = await ProductsService()
-                        .getShoppingCartList(context);
-                    // Check if we can pop (meaning there's a previous screen)
-                    if (Navigator.canPop(context)) {
-                      // Pop to see what's behind, but we'll need to be smart about this
-                      Navigator.pop(context);
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (widget.product.brand != null)
+                    CustomPlayFairText(
+                      fontSize: 22,
+                      text: widget.product.brand!,
+                    ),
+                  Text(
+                    widget.product.title,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    widget.product.category.replaceAll("-", " ").toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Color(0xff848484),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  PriceText(
+                    price: widget.product.price,
+                    discountPercentage: widget.product.discountPercentage,
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 95),
+                    child: BuyNowButton(
+                      onTap: () async {
+                        await ProductsService().addProductToShoppingCart(
+                          context,
+                          widget.product,
+                        );
+                        final productsMap = await ProductsService()
+                            .getShoppingCartList(context);
+                        // Check if we can pop (meaning there's a previous screen)
+                        if (Navigator.canPop(context)) {
+                          // Pop to see what's behind, but we'll need to be smart about this
+                          Navigator.pop(context);
 
-                      // After popping, check if current screen is checkout
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        final newCurrentRoute = ModalRoute.of(context);
+                          // After popping, check if current screen is checkout
+                          WidgetsBinding.instance.addPostFrameCallback((_) {
+                            final newCurrentRoute = ModalRoute.of(context);
 
-                        // If the route we just revealed has checkout settings, don't push again
-                        if (newCurrentRoute?.settings.name == 'checkout' ||
-                            newCurrentRoute?.settings.arguments
-                                is Map<ProductModel, int>) {
-                          // Previous was checkout, so we're good - just stay here
-                          return;
+                            // If the route we just revealed has checkout settings, don't push again
+                            if (newCurrentRoute?.settings.name == 'checkout' ||
+                                newCurrentRoute?.settings.arguments
+                                    is Map<ProductModel, int>) {
+                              // Previous was checkout, so we're good - just stay here
+                              return;
+                            } else {
+                              // Previous wasn't checkout, so push checkout
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (context) => CheckoutView(
+                                        productsMap: productsMap,
+                                      ),
+                                  settings: const RouteSettings(
+                                    name: 'checkout',
+                                  ),
+                                ),
+                              );
+                            }
+                          });
                         } else {
-                          // Previous wasn't checkout, so push checkout
+                          // No previous screen, so just push checkout
                           Navigator.pushReplacement(
                             context,
                             MaterialPageRoute(
@@ -87,37 +121,52 @@ class ProductDetailsSection extends StatelessWidget {
                             ),
                           );
                         }
-                      });
-                    } else {
-                      // No previous screen, so just push checkout
-                      Navigator.pushReplacement(
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  ClickableModernUnderlinedText(
+                    text: "add to cart",
+                    underlineWidth: 110,
+                    onTap: () async {
+                      await ProductsService().addProductToShoppingCart(
                         context,
-                        MaterialPageRoute(
-                          builder:
-                              (context) =>
-                                  CheckoutView(productsMap: productsMap),
-                          settings: const RouteSettings(name: 'checkout'),
-                        ),
+                        widget.product,
                       );
-                    }
-                  },
-                ),
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        cartIconKey.currentState?.refreshCart();
+                      });
+                    },
+                  ),
+                ],
               ),
-              const SizedBox(height: 15),
-              ClickableModernUnderlinedText(
-                text: "add to cart",
-                underlineWidth: 110,
-                onTap: () async {
-                  await ProductsService().addProductToShoppingCart(
-                    context,
-                    product,
-                  );
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    cartIconKey.currentState?.refreshCart();
-                  });
-                },
-              ),
-            ],
+            ),
+          ],
+        ),
+        Positioned(
+          right: 10,
+          top: 10,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                isFavorite = !isFavorite;
+              });
+              if (!isFavorite) {
+                ProductsService().removeProductFromFavList(
+                  context,
+                  widget.product,
+                );
+              } else {
+                ProductsService().addProductToFavList(context, widget.product);
+              }
+            },
+            child:
+                isFavorite
+                    ? const Icon(
+                      Icons.favorite,
+                      color: Color.fromARGB(255, 255, 118, 108),
+                    )
+                    : const Icon(Icons.favorite_border),
           ),
         ),
       ],
